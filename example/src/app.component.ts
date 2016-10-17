ba2b2ca13791a76e8a5b70cbb9b8b39932a08063
ZhihuiTian <tian.zhihui@intasect.co.jp>
2016-10-17 18:30:02 +0900

    change something.

--- example/src/app.component.ts
+++ example/src/app.component.ts
@@ -3,25 +3,25 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-    <div>
-        <h2></h2>
-        <ng2-gethtml>
-            <div id="test">
-                This will be change to 123.
-            </div>
-            <ng2-script>
-                console.log(1)
-            </ng2-script>
-            <ng2-script src="http://code.jquery.com/jquery-3.1.1.js"></ng2-script>
-            <ng2-script>
-                console.log(2)
-            </ng2-script>
-            <ng2-script>
-                $("#test").html("123");
-            </ng2-script>
-        </ng2-gethtml>
-    </div>
-  `,
+        <div>
+            <h2>ng2-gethtml example</h2>
+            <ng2-gethtml>
+                <div id="test">
+                    This will be change to 123.
+                </div>
+                <ng2-script>
+                    document.getElementById("test").style.color = "red";
+                </ng2-script>
+                <ng2-script src="http://code.jquery.com/jquery-3.1.1.js"></ng2-script>
+                <ng2-script>
+                    console.log(2)
+                </ng2-script>
+                <ng2-script>
+                    $("#test").html("123");
+                </ng2-script>
+            </ng2-gethtml>
+        </div>
+    `
 })
 export class AppComponent {
     constructor() {
--- src/ng2-script/ng2-script.component.ts
+++ src/ng2-script/ng2-script.component.ts
@@ -4,7 +4,7 @@ import { Component, AfterContentInit, Input, ElementRef } from '@angular/core';
     selector: 'ng2-script',
     providers: [
     ],
-    template: '<ng-content></ng-content>'
+    template: '<div style="display:none"><ng-content></ng-content></div>'
 })
 export class Ng2Script {
     @Input('src') src: string;
@@ -13,22 +13,26 @@ export class Ng2Script {
     }
 
     public load(): Promise<boolean> {
-        return new Promise<boolean>(resolve => {
-            let contentSource = this.element.nativeElement.innerText;
-            this.element.nativeElement.innerText = '';
-            if (this.src !== undefined && contentSource === '') {
-                let scriptNode = document.createElement('script');
-                scriptNode.setAttribute('type', 'text/javascript');
-                scriptNode.setAttribute('src', this.src);
-                scriptNode.onload = function () {
-                    scriptNode.onload = null;
-                    resolve(true);
-                };
-                this.element.nativeElement.append(scriptNode);
+        return new Promise<boolean>((resolve, reject) => {
+            if (this.element.nativeElement.childNodes[0]) {
+                let contentSource = this.element.nativeElement.childNodes[0].innerText;
+                this.element.nativeElement.innerText = '';
+                if (this.src !== undefined && contentSource === '') {
+                    let scriptNode = document.createElement('script');
+                    scriptNode.setAttribute('type', 'text/javascript');
+                    scriptNode.setAttribute('src', this.src);
+                    scriptNode.onload = function () {
+                        scriptNode.onload = null;
+                        resolve(true);
+                    };
+                    this.element.nativeElement.append(scriptNode);
 
+                } else {
+                    eval(contentSource);
+                    resolve(true);
+                }
             } else {
-                eval(contentSource);
-                resolve(true);
+                reject();
             }
         });
     }
